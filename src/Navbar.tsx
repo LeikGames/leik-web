b39dd7a4f4db96ece7122f4f03bb1844fd368a14
Oliver Andreas Thunæs <Felolis30@gmail.com>
2023-06-07 20:28:57 +0200

    Testing styling

---
+++ src/Navbar.tsx
@@ -0,0 +1,28 @@
+import "./Navbar.scss";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="container">
+        <div className="nav-elements">
+          <ul>
+            <li>
+              <a href="/">Test</a>
+            </li>
+            <li>
+              <a href="/">Test</a>
+            </li>
+            <li>
+              <a href="/">Test</a>
+            </li>
+            <li>
+              <a href="/">Test</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
