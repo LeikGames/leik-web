fe86a4e3296fbb8a1e682d39142d76b7ac9e143c
Oliver A. Thunæs <oliver.thunaes@gmail.com>
2025-04-30 15:15:22 +0200

    Added contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,15 @@ import { defineConfig } from 'astro/config'
 
 import icon from 'astro-icon'
 
+import vercel from '@astrojs/vercel'
+
 // https://astro.build/config
 export default defineConfig({
     integrations: [icon()],
+    adapter: vercel({
+        webAnalytics: {
+            enabled: true,
+        },
+    }),
 })
+
